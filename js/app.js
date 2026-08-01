e8b5360c530e8cda3c936b03bbba042741f5f2fc
Anthouy <anthonywudesign@gmail.com>
2026-08-01 02:23:25 -0400

    removed px and changed all to topnav

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+/* When the user scrolls down, hide the topnav. When the user scrolls up, show the topnav */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("topnav").style.top = "0";
   } else {
-    document.getElementById("topnav").style.top = "-50px";
+    document.getElementById("topnav").style.top = "-50";
   }
   prevScrollpos = currentScrollPos;
 }
