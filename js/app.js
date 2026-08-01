13486726834815370dc77410339026116cefb34d
Anthouy <anthonywudesign@gmail.com>
2026-08-01 02:26:22 -0400

    renamed topnav to navbar

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
-/* When the user scrolls down, hide the topnav. When the user scrolls up, show the topnav */
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("topnav").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("topnav").style.top = "-50";
+    document.getElementById("navbar").style.top = "-50";
   }
   prevScrollpos = currentScrollPos;
 }
