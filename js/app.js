8dd5197b4f5b0fdbd167f677a660150230f4b3f5
Anthouy <anthonywudesign@gmail.com>
2026-08-01 02:40:10 -0400

    added back px to js

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "0px";
   } else {
-    document.getElementById("navbar").style.top = "-50";
+    document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 }
